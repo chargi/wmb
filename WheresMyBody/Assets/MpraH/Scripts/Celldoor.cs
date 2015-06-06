c26e97b4e1441ec1e23a8f418001df8e86e1ca82
Markus Koller <markus.mprah.koller@gmail.com>
2015-06-06 21:53:06 +0200

    Added particle effects and replaced celldor animation with script rotation

---
+++ WheresMyBody/Assets/MpraH/Scripts/Celldoor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Celldoor : MonoBehaviour {
+	//private Animator animator;
+	private bool open=false;
+	private float turnSpeed=30f;
+	
+	void Start(){
+		//animator=GetComponent<Animator>();
+	}
+	
+	void Update(){
+		if(open && transform.eulerAngles.y<160){
+			transform.Rotate(new Vector3(0,1,0), turnSpeed*Time.deltaTime);
+		}
+	}
+	
+	void OnMouseDown(){
+		//animator.Play("Open");
+		open=true;
+	}
+	
+}
