5354ba02af08f105b353fe26d9b5a3792e51db3f
Markus Koller <markus.mprah.koller@gmail.com>
2015-06-13 17:54:17 +0200

    2nc commit for cog movement

---
+++ WheresMyBody/Assets/MpraH/Scripts/CogE.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CogE : MonoBehaviour {
+
+	private bool run=true;
+	private float turnSpeed=40f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(run)
+			transform.Rotate(new Vector3(0,0,-1), turnSpeed*Time.smoothDeltaTime);
+	}
+}
