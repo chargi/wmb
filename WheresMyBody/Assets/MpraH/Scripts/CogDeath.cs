697a620178094083807b7594a84b15420fe10960
Markus Koller <markus.mprah.koller@gmail.com>
2015-07-03 18:29:04 +0200

    Final mini changes

--- WheresMyBody/Assets/MpraH/Scripts/CogDeath.cs
+++ WheresMyBody/Assets/MpraH/Scripts/CogDeath.cs
@@ -20,11 +20,11 @@ public class CogDeath : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(!died&& transform.position.z>-38 && transform.position.z<-28 && transform.position.y<18 && transform.position.y>10 && transform.position.x<16 && transform.position.x>10){
-			died=true;
 			cam2.transform.rotation=Quaternion.Euler(0,-90,0);
 			cam2.transform.Translate(new Vector3(0,0,-5));
 			god.Die();
 			Instantiate(twisted, new Vector3(9.49f, 10.6f, -31.5f), Quaternion.Euler(-90,-90,0));
+			died=true;
 		}
-	}
+	}	
 }
